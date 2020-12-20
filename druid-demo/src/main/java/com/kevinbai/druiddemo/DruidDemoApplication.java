0ddb42a8d9bd295c711409651adda082829701b1
kevinbai <kevinbai.cn@gmail.com>
2020-12-20 23:24:28 +0800

    Add druid-demo

---
+++ druid-demo/src/main/java/com/kevinbai/druiddemo/DruidDemoApplication.java
@@ -0,0 +1,26 @@
+package com.kevinbai.druiddemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Slf4j
+public class DruidDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDemoApplication.class, args);
+    }
+
+    public void run(String... args) {
+        log.info(dataSource.toString());
+    }
+
+}
